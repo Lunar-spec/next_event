1a3a2e9c2e27ce3632b594c95a59bdf202a96bc4
Manjesh Singh <smanjesh1854@gmail.com>
2024-01-01 19:09:32 +0530

    minor fixes

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,23 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-let cached = (global as any).mongoose || { conn: null, promise: null }
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDb = async () => {
     if (cached.conn) return cached.conn;
 
-    console.log('running');
-    
-
-    if (!MONGODB_URL) throw new Error('MONGO_URL MISSING')
+    if (!MONGODB_URL) throw new Error('MONGODB_URL is missing');
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-        dbName: 'ArcTic',
-        bufferCommands: false
+        dbName: 'arctik',
+        bufferCommands: false,
     })
 
     cached.conn = await cached.promise;
 
     return cached.conn;
 }
+
